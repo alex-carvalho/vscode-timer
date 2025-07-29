eadee2d9b699443bee931d302d09939522dc9080
Alex Carvalho <alexcarvalhoandres@gmail.com>
2025-07-29 09:38:23 +0900

    Add unit timer selection

--- README.md
+++ README.md
@@ -7,15 +7,3 @@ It could be helpfull for pomodoro technique or Dojo sessions.
 
 Start, pause, reset a timer.
 
-Configure
-![Configure timer](https://i.ibb.co/Kp5qCWfz/configure-timer.png)
-
-<br>
-
-Set timer
-![Set timer](https://i.ibb.co/vvCygq0D/set-timer.png)
-
-<br>
-
-Timer running
-![Timer running](https://i.ibb.co/4RSLG3Ct/timer-running.png)
--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,11 @@ export function activate(context: vscode.ExtensionContext) {
 	const timer = new Timer(context);
 
 	const setTimerCommand = vscode.commands.registerCommand('timer.setTimer', async () => {
+		const config = vscode.workspace.getConfiguration();
+		const unit = config.get<string>('timer.unit', 'seconds');
+		const prompt = `Enter timer duration in ${unit}. (use preference setting to change unit)`;
 		const value = await vscode.window.showInputBox({
-			prompt: 'Enter timer duration in seconds',
+			prompt,
 			validateInput: (input) => {
 				const n = Number(input);
 				if (isNaN(n) || n <= 0) {
@@ -18,7 +21,11 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 		if (value) {
-			timer.setTimer(Number(value));
+			let seconds = Number(value);
+			if (unit === 'minutes') {
+				seconds = seconds * 60;
+			}
+			timer.setTimer(seconds);
 		}
 	});
 	context.subscriptions.push(setTimerCommand);
